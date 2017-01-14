75685b5ddc291c5000b077e9ac9e774983e342d1
Yining Shi <ys2189@nyu.edu>
2017-01-14 02:22:32 -0500

    permutation

---
+++ permutation.js
@@ -0,0 +1,42 @@
+// Given a collection of distinct numbers, return all possible permutations.
+
+// For example,
+// [1,2,3] have the following permutations:
+// [
+//   [1,2,3],
+//   [1,3,2],
+//   [2,1,3],
+//   [2,3,1],
+//   [3,1,2],
+//   [3,2,1]
+// ]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    var results = [];
+    
+    if (nums === null) {
+        return results;
+    }
+    
+    permuteHelper(results, [], nums);
+    
+    return results;
+};
+
+function permuteHelper(results, subset, nums) {
+    for (var i = 0; i < nums.length; i++) {
+        if (subset.indexOf(nums[i]) === -1) {
+            subset.push(nums[i]);
+            permuteHelper(results, subset, nums);
+            subset.pop();
+        }
+    }
+    
+    if (subset.length === nums.length) {
+        results.push(subset.slice());
+    }
+}
