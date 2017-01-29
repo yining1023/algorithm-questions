669eda3b9e0d3b4b8479637512a2fa8aafa066f4
Yining Shi <ys2189@nyu.edu>
2017-01-28 23:44:31 -0500

    dfs on bt

--- 117. Populating Next Right Pointers in Each Node I.js
+++ 117. Populating Next Right Pointers in Each Node I.js
@@ -90,6 +90,49 @@
 //     }
 // }
 
+// java
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeLinkNode parent = root;
+        TreeLinkNode pre;
+        TreeLinkNode next;
+        while (parent != null) {
+            pre = null;
+            next = null;
+            while (parent != null) {
+                if (next == null){
+                    next = (parent.left != null) ? parent.left: parent.right;
+                }
+
+                if (parent.left != null){
+                    if (pre != null) {
+                        pre.next = parent.left;
+                        pre = pre.next;
+                    } else {
+                        pre = parent.left;
+                    }
+                }
+
+                if (parent.right != null) {
+                    if (pre != null) {
+                        pre.next = parent.right;
+                        pre = pre.next;
+                    } else {
+                        pre = parent.right;
+                    }
+                }
+                parent = parent.next;
+            }
+            parent = next;
+        }
+    }
+}
+
+// Javascript
 var connect = function(root) {
     if(!root) {
         return;
