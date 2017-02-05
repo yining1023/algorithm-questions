d0c300c2e6452935c774a1da24c0051e956e91f9
Yining Shi <ys2189@nyu.edu>
2017-02-05 15:16:08 -0500

    bfs

--- 102. Binary Tree Level Order Traversal.js
+++ 102. Binary Tree Level Order Traversal.js
@@ -1,3 +1,9 @@
+// 102. Binary Tree Level Order Traversal
+// Description  Submission  Discussion  Add to List
+// Total Accepted: 150914
+// Total Submissions: 404065
+// Difficulty: Medium
+// Contributors: Admin
 // Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
 
 // For example:
@@ -13,6 +19,9 @@
 //   [9,20],
 //   [15,7]
 // ]
+// Hide Company Tags LinkedIn Facebook Amazon Microsoft Apple Bloomberg
+// Hide Tags Tree Breadth-first Search
+// Hide Similar Problems (M) Binary Tree Zigzag Level Order Traversal (E) Binary Tree Level Order Traversal II (E) Minimum Depth of Binary Tree (M) Binary Tree Vertical Order Traversal
 
 /**
  * Definition for a binary tree node.
@@ -25,44 +34,74 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-
-// use queue to first in first out to push them into temp
 var levelOrder = function(root) {
-    results = [];
+    let results = [];
     if (root === null) {
         return results;
     }
     
-    var queue = [],
-        temp = [],
-        curCount = 1,
-        nextLevCount = 0;
-        
+    let queue = [];
     queue.push(root);
     
-    while (queue.length !== 0) {
-        var p = queue.shift();
-        // push p.val not p itself!! p is a treeNode
-        temp.push(p.val);
-        curCount--;
-        
-        if (p.left) {
-            queue.push(p.left);
-            nextLevCount++;
+    while(queue.length > 0) {
+        let size = queue.length;
+        let level = [];
+        for (let i = 0; i < size; i++) {
+            let head = queue.shift();
+            // push its val not the whole tree node!!!
+            level.push(head.val);
+            if (head.left !== null) {
+                queue.push(head.left);
+            }
+            if (head.right !== null) {
+                queue.push(head.right);
+            }
         }
-        
-        if (p.right) {
-            queue.push(p.right);
-            nextLevCount++;
-        }
-        
-        if (curCount === 0) {
-            results.push(temp);
-            temp = [];
-            curCount = nextLevCount;
-            nextLevCount = 0;
-        }
+        results.push(level);
     }
     
     return results;
-};
+}
+
+// BFS!!
+// var levelOrder = function(root) {
+//     results = [];
+//     if (root === null) {
+//         return results;
+//     }
+    
+//     var queue = [],
+//         temp = [],
+//         curCount = 1,
+//         nextLevCount = 0;
+        
+//     queue.push(root);
+    
+//     while (queue.length !== 0) {
+//         var p = queue.shift();
+//         // push p.val not p itself!! p is a treeNode
+//         temp.push(p.val);
+//         curCount--;
+        
+//         if (p.left) {
+//             queue.push(p.left);
+//             nextLevCount++;
+//         }
+        
+//         if (p.right) {
+//             queue.push(p.right);
+//             nextLevCount++;
+//         }
+        
+//         // means the level is over
+//         if (curCount === 0) {
+//             results.push(temp);
+//             // reset
+//             temp = [];
+//             curCount = nextLevCount;
+//             nextLevCount = 0;
+//         }
+//     }
+    
+//     return results;
+// };
