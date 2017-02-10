a3874a1f5d73964966e345d57c0750e0cd85b9c3
Yining Shi <ys2189@nyu.edu>
2017-02-10 04:13:01 -0500

    copy list with random pointers

--- 138. Copy List with Random Pointer.js
+++ 138. Copy List with Random Pointer.js
@@ -21,24 +21,136 @@
  * @return {RandomListNode}
  */
 
-// 这题的关键是如何track一个节点是否已经被copy了。假如我们要copy如下list，用指针p1来扫描每个节点，另一个指针p2建立copy。
-// ______
-// |           |
-// |          V
-// 1->2->3
+这题的关键是如何track一个节点是否已经被copy了。假如我们要copy如下list，用指针p1来扫描每个节点，另一个指针p2建立copy。
+______
+|     |
+|     V
+1->2->3
 
-// p1扫描1时，p2复制1，以及1->next (2), 1->random (3)。之后p1, p2分别移到各自的2节点。
-// 此时我们必须得知道节点3在之前已经被复制了，并且得知道复制节点的地址。
-// ______
-// |           |
-// |          V
-// 1->2    3 
+p1扫描1时，p2复制1，以及1->next (2), 1->random (3)。之后p1, p2分别移到各自的2节点。
+此时我们必须得知道节点3在之前已经被复制了，并且得知道复制节点的地址。
+______
+|       |
+|       V
+1->2    3 
 
-// 所以这里可以使用一个hash table来记录原节点和复制节点的地址对应关系。这样每次要建立当前节点p的next和random前，先在hash table中查找。如果找到，则直接连接；否则建立新节点连上，并把和原节点的对应关系存入hash table中。
+所以这里可以使用一个hash table来记录!!原节点!!和!!复制节点!!的地址对应关系。
+这样每次要建立当前节点p的next和random前，先在hash table中查找。
+如果找到，则直接连接；否则建立新节点连上，并把和原节点的对应关系存入hash table中。
+/**
+ * Definition for singly-linked list with a random pointer.
+ * function RandomListNode(label) {
+ *     this.label = label;
+ *     this.next = this.random = null;
+ * }
+ */
+
+/**
+ * @param {RandomListNode} head
+ * @return {RandomListNode}
+ */
  
+/*第一遍扫的时候巧妙运用next指针， 开始数组是1->2->3->4  。 然后扫描过程中 先建立copy节点 1->1`->2->2`->3->3`->4->4`, 然后第二遍copy的时候去建立边的copy， 拆分节点, 一边扫描一边拆成两个链表，这里用到两个dummy node。第一个链表变回  1->2->3 , 然后第二变成 1`->2`->3`  */
+//No HashMap version
+var copyRandomList = function(head) {
+    if (head === null) {
+        return null;
+    }
+    
+    copyNodes(head);
+    copyRandom(head);
+    return splitList(head);
+    
+}
+
+function copyNodes(head) {
+    while (head !== null) {
+        // copy its label, next, random
+        newNode = new RandomListNode(head.label);
+        newNode.next = head.next;
+        newNode.random = head.random;
+        // insert
+        head.next = newNode;
+        // move one more step
+        head = head.next.next;
+    }
+}
+
+function copyRandom(head) {
+    while (head !== null) {
+        if (head.next.random !== null) {
+            head.next.random = head.random.next;
+        }
+        head = head.next.next;
+    }
+    
+}
+
+function splitList(head) {
+    let newHead = head.next;
+    while(head !== null) {
+        // 想让head.next跳一个，先要存下来head.next
+        let temp = head.next;
+        head.next = temp.next;
+        // head move to next
+        head = head.next;
+        // temp is not the last node
+        if (temp.next !== null) {
+            temp.next = temp.next.next;
+        }
+    }
+    return newHead;
+}
+
+
+// version 2: hashmap 
+// because it has random pointer, in order to avoid to copy duplicates node,
+// we need hashmap to save the relationship between the old and new nodes
+// javascript cannot use an object as a key in the hash, so save head.label as key!!!!
+var copyRandomList = function(head) {
+    if (head === null) {
+        return null;
+    }
+    
+    let dummy = new RandomListNode(0);
+    let pre = dummy, newNode;
+    let hash = {};
+    while (head !== null) {
+        // when there is no key in the hash
+        if (!hash[head.label] || hash[head.label] === undefined) {
+            // deep copy the node
+            // head.label, not head.val
+            newNode = new RandomListNode(head.label);
+            hash[head.label] = newNode;
+        } else {
+            newNode = hash[head.label];
+        }
+        
+        // copy random pointer
+        if (head.random !== null) {
+            if (!hash[head.random.label] || hash[head.random.label] === undefined) {
+                newNode.random = new RandomListNode(head.random.label);
+                hash[head.random.label] = newNode.random;
+            } else {
+                // 如果已经new了这个node，不能重复new，只是把.random的关系连上
+                newNode.random = hash[head.random.label];
+            }
+        }
+        
+        pre.next = newNode;
+        // move to next
+        pre = newNode;
+        head = head.next;
+    }
+    
+    return dummy.next;
+}
+
+
 // deep copy the entire linked list
 // copy itself first, then worry about its random
 // then use Hashmap to track the points that already gone through
+// HashMap is like that key: [node.label], value: the whole node!
 var copyRandomList = function(head) {
     var hashMap = {};
     var newHead = new RandomListNode(0);
@@ -49,13 +161,18 @@ var copyRandomList = function(head) {
             return node;
         }
         
+        // if already went through this one, return its node
         if (hashMap[node.label]) {
             return hashMap[node.label];
         }
         
+        // copy the node itself
         var newNode = new RandomListNode(node.label);
+        
+        // save into hashmap
         hashMap[node.label] = newNode;
         
+        // copy its next and random
         newNode.next = copyList(node.next);
         newNode.random = copyList(node.random);
         
@@ -64,4 +181,3 @@ var copyRandomList = function(head) {
     
     return newHead.next;
 };
-
--- 53. Maximum Subarray.js
+++ 53. Maximum Subarray.js
@@ -17,8 +17,53 @@
  * @param {number[]} nums
  * @return {number}
  */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ 
+// prefix sum
+var maxSubArray = function(nums) {
+    if (nums === null || nums.length === 0) {
+        return;
+    }
+    
+    let sum = 0;
+    let maxSum = -Infinity;
+    // minSum = 0 at first
+    let minSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        // 求prefix sum的最大点和最小点的距离
+        maxSum = Math.max(maxSum, sum - minSum);
+        minSum = Math.min(minSum, sum);
+    }
+    return maxSum;
+}
 
-// slide window!!!!
+// greedy
+var maxSubArray = function(nums) {
+    // O(n)
+    if (nums === null || nums.length === 0) {
+        return;
+    }
+    var sum = 0,
+        maxSum = -Infinity;
+
+    for (var i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        maxSum = Math.max(maxSum, sum);
+        
+        // when sum < 0, reset sum, means that start from the next number, why?? greedy
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    
+    return maxSum;
+};
+
+// slide window!!!! greedy
 var maxSubArray = function(nums) {
     // O(n)
     if (nums === null || nums.length === 0) {
