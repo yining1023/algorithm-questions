950336f3b49f08198e7fd39514751c5552f8a748
Yining Shi <ys2189@nyu.edu>
2017-02-20 06:01:30 -0500

    lrc

--- 146. LRU Cache.js
+++ 146. LRU Cache.js
@@ -25,4 +25,92 @@
 // Hide Tags Design
 // Hide Similar Problems (H) LFU Cache
 
+/**
+ * @param {number} capacity
+ */
+// hash map + linked list with prev and next!!
+// dummy head and dummy tail, because double link, has head, and tail, might delete head, or tail, needs dummy
+// having a function for move to the tail
+// get: remove from the list, move to the tail
+// set: if size === capacity, delete head in list and map, add new one, move to tail
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+var LRUCache = function(capacity) {
+    this.map = new Map();
+    this.capacity = capacity;
+    
+    this.dummyHead = new Node(-1, -1);
+    this.dummyTail = new Node(-1, -1);
+    
+    this.dummyHead.next = this.dummyTail;
+    this.dummyTail.prev = this.dummyHead;
 
+    this.move_to_tail = function(current) {
+        // sth is changed, cannot use it to get the reference
+        current.prev = this.dummyTail.prev;
+        this.dummyTail.prev = current;
+        current.prev.next = current;
+        current.next = this.dummyTail;
+    };
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (this.map.get(key) === undefined || !this.map.get(key)) {
+        return -1;
+    }
+    
+    // remove it from the list
+    let current = this.map.get(key);
+    current.prev.next = current.next;
+    current.next.prev = current.prev;
+    
+    // move to the tail
+    this.move_to_tail(current);
+
+    return this.map.get(key).value;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    // 保证是存在在cache里面！！！！
+    if (this.get(key) !== -1) {
+        this.map.get(key).value = value;
+        return;
+    }
+    
+    if (this.map.size === this.capacity) {
+        // delete the key
+        // OMG OMG OMG F*CK
+        // not delete key!!!! delete the previous head.key!!!!!!!!!!!!!!!!!!!!!!
+        this.map.delete(this.dummyHead.next.key);
+
+        // delete the head
+        this.dummyHead.next = this.dummyHead.next.next;
+        this.dummyHead.next.prev = this.dummyHead;
+    }
+    
+    let insert = new Node(key, value);
+    this.map.set(key, insert);
+    this.move_to_tail(insert);
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = Object.create(LRUCache).createNew(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
--- strStr.js
+++ strStr.js
@@ -7,28 +7,30 @@
  * @return {number}
  */
 
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+// O(N + M)
+// hash code for each substring, compare it to the target's hash code.
+
 // O(NM)
 var strStr = function(haystack, needle) {
-  // no need to check if they are strings
   if (haystack === null || needle === null) {
       return -1;
   }
 
-  // stop i in advance
   for (var i = 0; i < haystack.length - needle.length + 1; i++) {
     var j;
-    // two for loops
     for (j = 0; j < needle.length; j++) {
       if (haystack[i+j] !== needle[j]) {
-        break; // if one doesn't match, break
+        break;
       }
     }
-    // the final state, j === needle.length
     if (j === needle.length) {
       return i;
     }
   }
-  // return -1 has to be out of the for loop
   return -1;
 };
-
