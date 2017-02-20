3953e35ca45db3af637ef51184c8e90237880187
Yining Shi <ys2189@nyu.edu>
2017-02-19 19:34:52 -0500

    two stacks

--- 394. Decode String.js
+++ 394. Decode String.js
@@ -21,10 +21,55 @@ Hide Similar Problems (H) Encode String with Shortest Length
  * @param {string} s
  * @return {string}
  */
-// 利用栈（Stack）数据结构。
-// 当出现左括号时，将字符串压栈。
-// 当出现右括号时，将字符串弹栈，加上 新的栈顶元素 * count。
-// 出现字母时，当前result为字母
+// recursion, split string by "[" call itself "]", recursion, and concat, might stack over flow, too deep
+// stack
+// one number stack, one string stack, one result to hold the current [...] reesult
+// loop through this string:
+// if a number: try find the complete digit, push into number stack
+// if '[', push the old result in, reset result to ''. result might be empty if there is no ] yet, prepare to go into one level deeper, save the current level first.
+// if ']', pop num, pop str, str += result x num, result = str. untangle this level!
+// if a letter, push into result
+var decodeString = function(s) {
+    if (s === null || s.length === 0) {
+        return '';
+    }
+    let strStack = [],
+        numStack = [],
+        result = '',
+        i = 0;
+    
+    while (i < s.length) {
+        if (Number.isInteger(parseInt(s[i]))) {
+            let number = 0;
+            // inner while has to add the condition that the outter while has
+            while (i < s.length && Number.isInteger(parseInt(s[i]))) {
+                number = number * 10 + (s.charAt(i) - '0');// number = number * 10 + new digit!
+                i++;
+            }
+            numStack.push(number);//push the number in, don't leave it empty()
+        } else if (s[i] === '[') {
+            strStack.push(result);
+            result = '';
+            i++;
+        } else if (s[i] === ']') {
+            let string = strStack.pop();
+            let count = numStack.pop();
+            for (let j = 0; j < count; j++) {
+                string = string.concat(result);
+            }
+            result = string;
+            i++;
+        } else {
+            result += s.charAt(i);
+            i++;
+        }
+    }
+    return result;
+};
+
+利用栈（Stack）数据结构。
+当出现左括号时，将字符串压栈。
+当出现右括号时，将字符串弹栈，并重复响应次数，累加至新的栈顶元素。
 
 var decodeString = function(s) {
     let stackStr = [];
