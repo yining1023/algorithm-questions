e8467eb6a035c35ff6c66b6e7cbec794df0eee3f
Yining Shi <ys2189@nyu.edu>
2017-03-22 16:00:23 -0400

    binary search

--- 162. Find Peak Element.js
+++ 162. Find Peak Element.js
@@ -9,26 +9,55 @@
 // For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
 
 // click to show spoilers.
+
+Note:
+Your solution should be in logarithmic complexity.
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
+
+// brute force O(N), for, check prev, cur, next value
+// binary seach O(logN)
+// 4 case: low point, high point, left lower right higher, left higher left lower
+var findPeakElement = function(nums) {
+    if (nums === null || nums.length === 0) {
+        return -1;
+    }
+
+    let start = 0,
+        end = nums.length - 1,
+        mid;
+
+    while (start + 1 < end) {
+        mid = start + parseInt((end - start) / 2);// parseInt!//use the bracket correctly!!
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;// peak//only return the index
+        else if (nums[mid] < nums[mid - 1] && nums[mid] > nums[mid + 1]) end = mid;// going down, go to left
+        else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) start = mid;// going up, go to right
+        else start = mid;
+    }
+
+    // in the end, only start and end left, manually handle them
+    return nums[start] > nums[end] ? start : end;// only return index, not the real number
+};
+
 
 // four cases, bottom, peak, going up, going down
 var findPeakElement = function(nums) {
     if (nums === null || nums.length === 0) {
         return -1;
     }
-    
+
     // end = numd.length - 1, don'r forget to - 1!!!!!
     var start = 0,
         end = nums.length - 1,
         mid;
-    
+
     while(start + 1 < end) {
         // parseInt use () to wrap all the numbers!!!!!
         mid = start + parseInt((end - start) / 2);
-        
+
         if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
             return mid;
         // it's nums[mid], not just nums!!!!!!!
@@ -40,7 +69,7 @@ var findPeakElement = function(nums) {
             start = mid;
         }
     }
-    
+
     return nums[start] - nums[end] > 0 ? start : end;
-    
+
 };
