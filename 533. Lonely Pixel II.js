1eb48e80867073509e0ec3a8b886561efb8ae430
Yining Shi <ys2189@nyu.edu>
2017-03-29 15:00:14 -0400

    row col, map, colCount

--- 533. Lonely Pixel II.js
+++ 533. Lonely Pixel II.js
@@ -54,6 +54,30 @@ Hide Similar Problems (M) Lonely Pixel II
 // For above example, only the first signature "WBWBBW" has count == 3. We validate 3 column 1, 3, 4 where char == 'B', and column 1 and 3 have 3 'B', then answer is 2 * 3 = 6.
 
 // need a hashmap like {"WBWBBW": 3, "WWBWBB": 1}, and a colCount [0, 3, 1, 3, 4, 1]
+/**
+ * @param {character[][]} picture
+ * @param {number} N
+ * @return {number}
+ */
+// O(m*n) Solution, HashMap
+
+// Scan each row. If that row has N black pixels, put a string signature, which is concatenation of each characters in that row, as key and how many times we see that signature into a HashMap. Also during scan each row, we record how many black pixels in each column in an array colCount and will use it in step 2.
+// For input:
+// [['W', 'B', 'W', 'B', 'B', 'W'],
+// ['W', 'B', 'W', 'B', 'B', 'W'],
+// ['W', 'B', 'W', 'B', 'B', 'W'],
+// ['W', 'W', 'B', 'W', 'B', 'B']]
+// We will get a HashMap:
+// {"WBWBBW": 3, "WWBWBB": 1}
+// and colCount array:
+// [0, 3, 1, 3, 4, 1]
+// Go through the HashMap and if the count of one signature is N, those rows potentially contain black pixels we are looking for. Then we validate each of those columns. For each column of them has N black pixels (lookup in colCount array), we get N valid black pixels.
+// For above example, only the first signature "WBWBBW" has count == 3. We validate 3 column 1, 3, 4 where char == 'B', and column 1 and 3 have 3 'B', then answer is 2 * 3 = 6.
+
+// need a hashmap like {"WBWBBW": 3, "WWBWBB": 1}, and a colCount [0, 3, 1, 3, 4, 1]
+
+// 1. 首先算每一行的b的个数，要是===n，放入map中
+// 2. loop整个map，看colcount[]符不符合===n
 var findBlackPixel = function(picture, N) {
     let m = picture.length,
         n = picture[0].length;
@@ -61,25 +85,32 @@ var findBlackPixel = function(picture, N) {
     let map = new Map();
     let colCount = new Array(n).fill(0);
 
-    for (let i = 0; i < m; i++) {
+    for (let i = 0; i < m; i++) { // 每一行的b的个数也要算上呀
         let curRowKey = '';
+        let curRowCount = 0;
         for (let j = 0; j < n; j++) {
             curRowKey += picture[i][j];
-            if (picture[i][j] === 'B') colCount[j]++;
+            if (picture[i][j] === 'B') {
+                colCount[j]++;
+                curRowCount++;
+            }
         }
-        if (map.has(curRowKey)) map.set(curRowKey, map.get(curRowKey) + 1);
-        else map.set(curRowKey, 1);
-    }
+        if (curRowCount === N) {
+            if (map.has(curRowKey)) map.set(curRowKey, map.get(curRowKey) + 1);
+            else map.set(curRowKey, 1);
+        }
 
+    }
 
     let results = 0;
     map.forEach( (value, key) => {
         if (value === N) { // N is the input, not any value can do, has to be N
             for (let k = 0; k < n; k++) {
-                if (key[k] === 'B' && colCount[k] === value) results += value;
+                if (key[k] === 'B' && colCount[k] === value) results += N;
             }
         }
     });
 
     return results;
 };
+
